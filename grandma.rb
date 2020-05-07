f565a7559caecb41afe261ab44211470bca3b1a8
Lantz Warrick <betalantz@gmail.com>
2020-05-07 23:07:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,19 +12,27 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-# def speak_to_grandma(phrase)
-#   if phrase != phrase.upcase
-#     return "HUH?! SPEAK UP, SONNY!"
-#   elsif phrase == 'I LOVE YOU GRANDMA!'
-#     return "I LOVE YOU TOO PUMPKIN!"
-#   else
-#     return "NO, NOT SINCE 1938!"
-#   end
-# end
-
 def speak_to_grandma(phrase)
-  return "I LOVE YOU TOO PUMPKIN!" if phrase == 'I LOVE YOU GRANDMA!'
-  return "HUH?! SPEAK UP, SONNY!" if phrase != phrase.upcase
-  return "NO, NOT SINCE 1938!"
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == 'I LOVE YOU GRANDMA!'
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
 end
+
+def speak_to_grandma_ternary(phrase)
+  phrase != phrase.upcase ? "HUH?! SPEAK UP, SONNY!" : (phrase == 'I LOVE YOU GRANDMA!' ? "I LOVE YOU TOO PUMPKIN!" : "NO, NOT SINCE 1938!")
+end
+
+puts speak_to_grandma_ternary("what's for dinner")
+puts speak_to_grandma_ternary("I LOVE YOU GRANDMA!")
+puts speak_to_grandma_ternary("FIRE!")
+  
+# def speak_to_grandma(phrase)
+#   return "I LOVE YOU TOO PUMPKIN!" if phrase == 'I LOVE YOU GRANDMA!'
+#   return "HUH?! SPEAK UP, SONNY!" if phrase != phrase.upcase
+#   return "NO, NOT SINCE 1938!"
+# end
 
